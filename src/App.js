59bf2369837109eb387f36654f4ff1e60f99ed92
Reda Boumahdi <reda.boumahdi@haroldwaste.com>
2022-06-21 19:35:35 +0200

    clipboard

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ function App() {
   const onTextChange = (event) => {
     setText(event.target.value);
   }
-  const result = text.replace('t', 'ت')
+  const result = text.replaceAll('t', 'ت')
   return (
     <div className="App">
       <div>Welcome to tahramit, please type your text, the app will show you it's magic</div>
       <input value={text} onChange={onTextChange} />
       <div>output: {result}</div>
+      <button onClick={() => navigator.clipboard.writeText(result)}>copy to clipboard</button>
     </div>
   );
 }
